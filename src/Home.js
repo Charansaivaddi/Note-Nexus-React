3c37c86b2cc032123610002c8e51911263e8b758
Charansaivaddi <charansaivaddi@gmail.com>
2023-11-18 22:52:48 +0530

    Added Home.css

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>
+    <div className='body'>
       <hr />
       <section id="body">
         <p id="prod_name" href="#home">
